fca9bb26d60f2b68d5af1201a1e69d481a4951b6
Leini Bak <leinibak@gmail.com>
2025-11-27 21:02:18 +0100

    add pastoral letters

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./pdfConfig";
 import "./index.css"; // Tailwind 또는 CSS 파일
 
 // React 18 이상: createRoot 사용
