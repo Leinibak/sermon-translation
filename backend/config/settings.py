66ded4fae71e1f27c1c6caf389eef01c5aeaf0b5
Leini Bak <leinibak@gmail.com>
2026-04-18 20:22:08 +0200

    add logs folderfix: create logs directory for CI

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,6 +4,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 👇 여기 추가
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+os.makedirs(LOG_DIR, exist_ok=True)
+
 # 환경 구분
 ENVIRONMENT = os.getenv("DJANGO_ENV", "dev")
 
